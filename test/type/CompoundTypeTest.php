2b71197c24e6da6f064b98b7260e4530ae97b5a0
TimoLehnertz <57093983+TimoLehnertz@users.noreply.github.com>
2024-12-20 19:09:47 +0100

    removed test

--- test/type/CompoundTypeTest.php
+++ test/type/CompoundTypeTest.php
@@ -1,14 +1,10 @@
 <?php
+
 namespace test\type;
 
 use PHPUnit\Framework\TestCase;
 use TimoLehnertz\formula\Formula;
 use TimoLehnertz\formula\procedure\Scope;
-use TimoLehnertz\formula\type\CompoundType;
-use TimoLehnertz\formula\type\DateTimeImmutableType;
-use TimoLehnertz\formula\type\FloatType;
-use TimoLehnertz\formula\type\IntegerType;
-use TimoLehnertz\formula\type\NullType;
 
 class CompoundTypeTest extends TestCase {
 
@@ -18,24 +14,8 @@ class CompoundTypeTest extends TestCase {
 
   public function testTruthy(): void {
     $scope = new Scope();
-    $scope->definePHP(true, 'func', [$this,'funcTest']);
+    $scope->definePHP(true, 'func', [$this, 'funcTest']);
     $formula = new Formula('var a = func(); a = 0; return a;', $scope);
     $this->assertEquals(0, $formula->calculate()->toPHPValue());
   }
-
-  public function testAssignable(): void {
-    $typeA = CompoundType::buildFromTypes([new NullType(), new IntegerType(), new FloatType()]);
-    $typeB = CompoundType::buildFromTypes([new NullType(), new DateTimeImmutableType()]);
-    $this->assertTrue($typeB->assignableBy($typeA));
-  }
-
-  //   public function testEquals(): void {
-  //     $scope = new Scope();
-  //     $scope->definePHP(false, 'scope1', $scope);
-  //     $scope->definePHP(false, 'scope2', new Scope());
-  //     $formula = new Formula('scope1 == scope1', $scope);
-  //     $this->assertEquals(true, $formula->calculate()->toPHPValue());
-  //     $formula = new Formula('scope1 == scope2', $scope);
-  //     $this->assertEquals(false, $formula->calculate()->toPHPValue());
-  //   }
 }
